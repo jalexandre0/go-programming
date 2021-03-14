0a1159cdd946fd60c3ef7a1cd6567640a6dc2eca
Jefferson Santos <jefferson.alexandre@gmail.com>
2021-03-14 11:56:32 -0300

    Done, but not satisfied.

---
+++ section_05/5.06.go
@@ -0,0 +1,25 @@
+// # Hands on Exercises - Level 2 #6
+//
+// Using iota, create 4 constants for the last 4 years. Print the constant values.
+//
+// [answer](https://play.golang.org/p/ffzD0s7w4m)
+
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a = 2021 - iota
+	b
+	c
+	d
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
