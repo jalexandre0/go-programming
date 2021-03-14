edc0533e32e29ef7d4c741316d2798f34c7940d0
Jefferson Santos <jefferson.alexandre@gmail.com>
2021-03-14 18:03:55 -0300

    if statment

---
+++ section_07/7.06.go
@@ -0,0 +1,16 @@
+// # Hands on Exercises - Level 3 #6
+//
+// Create a program that shows the "if statement" in action.
+//
+// [answer](https://play.golang.org/p/DpZ_FLfn5s)
+
+package main
+
+import "fmt"
+
+func main() {
+	n := 42
+	if n >= 42 {
+		fmt.Println("42 equals 42")
+	}
+}
