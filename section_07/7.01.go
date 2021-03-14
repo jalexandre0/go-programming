4ed5370893e5825312747cf05143e3ccd1e2ddec
Jefferson Santos <jefferson.alexandre@gmail.com>
2021-03-14 17:23:32 -0300

    Done

---
+++ section_07/7.01.go
@@ -0,0 +1,17 @@
+// # Hands on Exercises - Level 3 #1
+//
+// Print every number from 1 to 10,000
+//
+// [answer](https://play.golang.org/p/Y7Tr8DqVuZ)
+
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i <= 10000; i++ {
+		fmt.Println(i)
+
+	}
+}
